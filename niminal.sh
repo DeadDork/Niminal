893023d9dd4707b6c21f8bd69f1667bfdca2a44c
eins <eins@niminal.(none)>
2012-06-03 05:40:53 -0700

    Niminal 0.2.3

--- niminal.sh
+++ niminal.sh
@@ -4,54 +4,60 @@
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install aptitude
 
-# While I get my act together with regards to niminal 0.2.0, niminal 0.1.0 will simply be a branch of lubuntu-core.
-sudo apt-get install lubuntu-core --no-install-recommends -y
+# I have decided to go hardcore, and NOT install lubuntu-core.
 
-# However, it comes with some tweaks of my own!
-sudo apt-get install\
+# Package and depends.
+sudo apt-get --no-install-recommends -y install\
+	anacron\
+	bc\
+	dbus-x11\
+	ghostscript-x\
+	inputattach\
+	libpam-ck-connector\
 	acpi-support\
+	wireless-tools\
+	wpasupplicant\
 	usb-modeswitch\
 	pcmciautils\
-	policykit-desktop-privileges\
 	desktop-file-utils\
-	gvfs-backends\
 	gvfs-fuse\
 	pm-utils\
 	update-notifier\
+	apport-gtk\
+	gdb\
 	x11-utils\
 	xdg-user-dirs\
-	audacious\
-	audacious-plugins\
+	xfe\
+	xarchiver\
+	p7zip-full\
+	cmus\
 	linux-headers-generic\
+	perl-doc\
 	ntp\
 	scrot\
 	xscreensaver\
+	xscreensaver-gl\
+	xscreensaver-gl-extra\
 	xfce4-power-manager\
 	rxvt-unicode-256color\
-	guake\
-	xterm\
-	elinks\
+	xfonts-terminus\
 	rdesktop\
 	zathura\
 	xclip\
-	cmus\
 	feh\
 	texlive-common\
-	libreoffice-calc\
+	libreoffice-base\
 	gimp\
 	seahorse\
-	vlc\
-	vlc-plugin-pulse\
+	mplayer2\
 	nmap\
 	encfs\
 	samba\
 	samba-common\
 	vim-gnome\
-	vim-doc\
 	unclutter\
-	xfonts-terminus\
-	scrot\
 	network-manager-gnome\
 	privoxy\
 	squid3\
@@ -59,18 +65,16 @@ sudo apt-get install\
 	mercurial\
 	cvs\
 	chromium-browser\
-	r-base\
-	ufw\
+	r-recommended\
+	molly-guard\
 	openssh-server\
-	openssh-client\
-	imagemagick\
 	pulseaudio\
+	gstreamer0.10-pulseaudio\
+	pulseaudio-module-x11\
 	parcellite\
-	trayer\
 	libwebkitgtk-dev\
 	libgtk2.0-dev\
 	libsoup2.4-dev\
-	mutt\
 	libc6-dev\
 	libao-dev\
 	libgnutls-dev\
@@ -78,87 +82,105 @@ sudo apt-get install\
 	libjson0-dev\
 	libfaad-dev\
 	libmad0-dev\
-	aptitude\
-	vim-gtk
+	mutt\
+	freetds-common\
+	rlwrap\
+	awesome\
+	awesome-extra\
+	foomatic-db-compressed-ppds\
+	foomatic-filters\
+	libsasl2-modules\
+	libxp6\
+	lightdm\
+	lightdm-gtk-greeter\
+	openprinting-ppds\
+	printer-driver-pnm2ppa\
+	rfkill\
+	ubuntu-extras-keyring\
+	xkb-data\
+	xorg\
+	avahi-daemon\
+	cups\
+	cups-bsd\
+	cups-client\
+	hplip\
+	kerneloops-daemon\
+	libnss-mdns\
+	printer-driver-c2esp\
+	printer-driver-foo2zjs\
+	printer-driver-min12xxw\
+	printer-driver-ptouch\
+	printer-driver-pxljr\
+	printer-driver-sag-gdi\
+	printer-driver-splix\
+	wvdial\
+	libpulse-mainloop-glib0\
+	blueman
 
-# Grab the dependencies for these projects as it is better to build them myself.
-sudo apt-get build-dep\
+# Package and depends and recommends.
+sudo aptitude -r install\
+	alsa-utils+\
+	policykit-desktop-privileges+\
+	gvfs-backends+\
+	audacious-plugins+
+
+# Package and depends, recommends and suggests.
+sudo apt-get --install-suggests install\
+	zip\
+	elinks
+
+# Only the depends.
+sudo apt-get --no-install-recommends build-dep\
 	xxxterm\
 	suckless-tools\
-	scrotwm\
-	freetds-common\
 	sqsh
 
 # Create directories.
-if [ -d "${HOME}/programming_projects" ]; then
-	:
-else
-	mkdir "${HOME}/programming_projects"
+if [ ! -d "${HOME}"/Programming/programming_projects ]; then
+	mkdir -p "${HOME}"/Programming/programming_projects
 fi
-if [ -d "${HOME}"/[Dd]ownloads ]; then
-	:
-else
-	mkdir "${HOME}"/downloads
+if [ ! -d "${HOME}"/Programming/programming_experiments ]; then
+	mkdir "${HOME}"/Programming/programming_experiments/{BASH,gawk,sed,SQL,hex,HTML,LaTeX,R,perl,python,sqsh}
 fi
-if [ -d "${HOME}"/[Dd]ocuments ]; then
-	:
-else
-	mkdir "${HOME}"/documents
+if [ ! -d "${HOME}"/[Dd]ownloads ]; then
+	mkdir "${HOME}"/Downloads
 fi
-if [ -d "${HOME}"/programming_experiments ]; then
-	:
-else
-	mkdir -p "${HOME}"/programming_experiments/{BASH,gawk,sed,SQL,hex,HTML,LaTeX,R,perl,python}
+if [ ! -d "${HOME}"/[Dd]ocuments ]; then
+	mkdir "${HOME}"/Documents
 fi
-if [ -d "${HOME}"/[Mm]anuals ]; then
-	:
-else
-	mkdir "${HOME}"/manuals
+if [ ! -d "${HOME}"/Documents/[Mm]anuals ]; then
+	mkdir "${HOME}"/Documents/manuals
 fi
 
 # Install various projects. These various projects need to be compiled and set by hand.
-cd "${HOME}/programming_projects"
-git clone git://opensource.conformal.com/xombrero.git
-#cd xombrero
-#make
-#sudo make install
-#cd ..
-git clone git://opensource.conformal.com/spectrwm.git
-#cd spectrwm
-#make
-#sudo make install
-#cd ..
-hg clone http://hg.suckless.org/st
-#cd st
-#make
-#sudo make install
-#cd ..
-hg clone http://hg.suckless.org/dmenu
-#cd dmenu
-#make
-#sudo make install
-#cd ..
-git clone git://github.com/PromyLOPh/pianobar.git
-#cd pianobar
-#make
-#sudo make install
-#cd ..
-git clone git://git.code.sf.net/p/vimprobable/code
-#cd vimprobable
-#make
-#sudo make install
-#cd ..
-cvs -d:pserver:anonymous@sqsh.cvs.sourceforge.net:/cvsroot/sqsh login 
-cvs -z3 -d:pserver:anonymous@sqsh.cvs.sourceforge.net:/cvsroot/sqsh co -P sqsh
-#cd sqsh
-#make
-#sudo make install
-#cd ..
+cd "${HOME}"/Programming/programming_projects
+if [ ! -d xombrero ]; then
+	git clone git://opensource.conformal.com/xombrero.git
+fi
+if [ ! -d spectrwm ]; then
+	git clone git://opensource.conformal.com/spectrwm.git
+fi
+if [ ! -d stterm ]; then
+	hg clone http://hg.suckless.org/st
+fi
+if [ ! -d dmenu ]; then
+	hg clone http://hg.suckless.org/dmenu
+fi
+if [ ! -d pianobar ]; then
+	git clone git://github.com/PromyLOPh/pianobar.git
+fi
+if [ ! -d vimprobable ]; then
+	git clone git://git.code.sf.net/p/vimprobable/code
+	mv code vimprobable
+fi
+if [ ! -d sqsh ]; then
+	cvs -d:pserver:anonymous@sqsh.cvs.sourceforge.net:/cvsroot/sqsh login 
+	cvs -z3 -d:pserver:anonymous@sqsh.cvs.sourceforge.net:/cvsroot/sqsh co -P sqsh
+fi
 
 # Need config files for:
 #	* elinks
 #	* vimprobable
-#	* rxvt
 #	* vim
 #	* samba
 #	* .xsession/.xinitrc
