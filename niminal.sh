12f72c5b507e80fb979d1335166cd368e52636c0
Nimrod Omer, aka DeadDork <nimrodomer@gmail.com>
2012-06-06 02:59:26 -0700

    This was the latest version of niminal.sh prior to my decision to split niminal into niminal-core and niminal-desktop. Since then, though, I've decided to merge the two back into niminal.sh.

--- niminal.sh
+++ niminal.sh
@@ -1,70 +1,97 @@
 #!/bin/bash
 
+# 0.1.x notes:
+# The 0.1.x version series took a minimal ubuntu install, and added to it preferred packages. This seems like a good idea for a minimal Linux distro based on Ubuntu, at least in theory, but in practice the final product was quite shoddy. For one, I was never confident that it was secure and stable. Second, it was quite ugly. Third, the UI left a lot to be desired.
+
+# 0.2.x notes:
+# In the 0.2.x version series, I cherry-picked Lubuntu packages to create a base install I could have some confidence in, then added to it a few choice packages. This series was plagued by problems, too: 1) Spectrwm proved to be a flop 2) while I had high confidence when using Lubuntu, when I logged in to Spectrwm I was never quite as sanguine.
+
+# 0.3.x notes:
+# I had hoped to fork the 0.3.x series, such that my custom base Lubuntu install would be configured to have the best of Awesome & Lubuntu. However, this turned out to entail a huge undertaking, so I'm going to leave that to the 0.4.x series (which I'll work on in conjunction with Cnaan over the next couple of months). Instead, the 0.3.x series will take my custom Lubuntu install, and I'll simply mod from within Lubuntu (e.g. changing to the Awesome WM from OpenBox via LXSession). I expect to use this series for the next 2 - 3 months.
+
 # Initial commands.
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install aptitude
 
-# I have decided to go hardcore, and NOT install lubuntu-core.
-
-# Package and depends.
+# Install just ackage and depends.
 sudo apt-get --no-install-recommends -y install\
-	anacron\
-	bc\
-	dbus-x11\
-	ghostscript-x\
-	inputattach\
-	libpam-ck-connector\
+	lubuntu-core\
 	acpi-support\
-	wireless-tools\
-	wpasupplicant\
-	usb-modeswitch\
+	avahi-daemon\
+	bluez\
+	bluez-alsa\
+	bluez-cups\
+	cups\
+	cups-bsd\
+	cups-client\
+	kerneloops-daemon\
+	laptop-detect\
+	libnss-mdns\
 	pcmciautils\
+	policy-kit-desktop-privileges\
 	desktop-file-utils\
+	gvfs-backends\
 	gvfs-fuse\
+	vim-gtk\
+	vim-latexsuite\
+	rxvt-unicode-lite\
 	pm-utils\
 	update-notifier\
-	apport-gtk\
-	gdb\
+	wvdial\
 	x11-utils\
 	xdg-user-dirs\
-	xfe\
-	xarchiver\
-	p7zip-full\
-	cmus\
+	audacious\
+	audacious-plugins\
+	apport-gtk\
+	blueman\
+	chromium-browser\
+	gnome-keyring\
+	gnome-mplayer\
+	gawk\
+	libreoffice-calc\
+	libreoffice-writer\
+	gpicview\
+	gucharmap\
+	guvcview\
+	ibus\
 	linux-headers-generic\
-	perl-doc\
+	lxrandr\
+	lxsession-edit\
+	modemmanager\
+	network-manager-gnome\
 	ntp\
 	scrot\
-	xscreensaver\
+	system-config-printer-gnome\
+	transmission\
+	usb-modeswitch\
+	xfce4-power-manager\
+	cmus\
+	perl-doc\
 	xscreensaver-gl\
 	xscreensaver-gl-extra\
-	xfce4-power-manager\
-	rxvt-unicode-256color\
 	xfonts-terminus\
 	rdesktop\
 	zathura\
 	xclip\
 	feh\
 	texlive-common\
-	libreoffice-base\
 	gimp\
 	seahorse\
-	mplayer2\
-	nmap\
 	encfs\
 	samba\
 	samba-common\
-	vim-gnome\
+	xscreensaver\
+	wireless-tools\
+	wpasupplicant\
+	gdb\
 	unclutter\
-	network-manager-gnome\
 	privoxy\
 	squid3\
 	git\
 	mercurial\
 	cvs\
-	chromium-browser\
 	r-recommended\
 	molly-guard\
 	openssh-server\
@@ -87,49 +114,17 @@ sudo apt-get --no-install-recommends -y install\
 	rlwrap\
 	awesome\
 	awesome-extra\
-	foomatic-db-compressed-ppds\
-	foomatic-filters\
-	libsasl2-modules\
-	libxp6\
-	lightdm\
-	lightdm-gtk-greeter\
-	openprinting-ppds\
-	printer-driver-pnm2ppa\
-	rfkill\
-	ubuntu-extras-keyring\
-	xkb-data\
-	xorg\
-	avahi-daemon\
-	cups\
-	cups-bsd\
-	cups-client\
-	hplip\
-	kerneloops-daemon\
-	libnss-mdns\
-	printer-driver-c2esp\
-	printer-driver-foo2zjs\
-	printer-driver-min12xxw\
-	printer-driver-ptouch\
-	printer-driver-pxljr\
-	printer-driver-sag-gdi\
-	printer-driver-splix\
-	wvdial\
-	libpulse-mainloop-glib0\
-	blueman
-
-# Package and depends and recommends.
-sudo aptitude -r install\
-	alsa-utils+\
-	policykit-desktop-privileges+\
-	gvfs-backends+\
-	audacious-plugins+
-
-# Package and depends, recommends and suggests.
-sudo apt-get --install-suggests install\
 	zip\
-	elinks
+	unzip\
+	elinks\
+	elinks-doc\
+	xarchiver\
+	gnash\
+	xli\
+	conky\
+	xfe
 
-# Only the depends.
+# Only install the depends.
 sudo apt-get --no-install-recommends build-dep\
 	xxxterm\
 	suckless-tools\
@@ -140,7 +135,7 @@ if [ ! -d "${HOME}"/Programming/programming_projects ]; then
 	mkdir -p "${HOME}"/Programming/programming_projects
 fi
 if [ ! -d "${HOME}"/Programming/programming_experiments ]; then
-	mkdir "${HOME}"/Programming/programming_experiments/{BASH,gawk,sed,SQL,hex,HTML,LaTeX,R,perl,python,sqsh}
+	mkdir "${HOME}"/Programming/programming_experiments/{BASH,awk,sed,SQL,hex,HTML,LaTeX,R,perl,python,sqsh}
 fi
 if [ ! -d "${HOME}"/[Dd]ownloads ]; then
 	mkdir "${HOME}"/Downloads
@@ -177,6 +172,13 @@ if [ ! -d sqsh ]; then
 	cvs -d:pserver:anonymous@sqsh.cvs.sourceforge.net:/cvsroot/sqsh login 
 	cvs -z3 -d:pserver:anonymous@sqsh.cvs.sourceforge.net:/cvsroot/sqsh co -P sqsh
 fi
+
+# Set firewall.
+sudo ufw default deny
+sudo ufw enable
+sudo ufw allow from 192.168.0.0/16 to any port 22
+sudo ufw allow proto tcp from 192.168.0.0/16 to any port 135,139,145
+sudo ufw allow proto udp from 192.168.0.0/16 to any port 137,138
 
 # Need config files for:
 #	* elinks
