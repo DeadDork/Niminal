06737c9f5e0d7515eb093c9c44cf196d7fb19fc5
DeadDork <nimrodomer@gmail.com>
2012-06-20 15:21:44 -0700

    Got rid of 'less' for added flexibility.

--- customs/scripts/process_snapshot.sh
+++ customs/scripts/process_snapshot.sh
@@ -2,6 +2,6 @@
 
 # This is a quick little script for showing what process is doing what when the script is called. I found that I was using the ps command per the below quite often, and simply opted to scriptify it for ease.
 
-ps aux | awk '(NR == 1){print} ((NR > 1) && ($3 > 0)){print} ((NR > 1) && ($4 > 0)){print}' | less
+ps aux | awk '(NR == 1){print} ((NR > 1) && ($3 > 0)){print} ((NR > 1) && ($4 > 0)){print}'
 
 exit 0
