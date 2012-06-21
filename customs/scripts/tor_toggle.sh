82c0c218782861010a7c124239b5010389d6d7ab
DeadDork <nimrodomer@gmail.com>
2012-06-21 00:36:42 -0700

    tor_toggle echoes tor status.

--- customs/scripts/tor_toggle.sh
+++ customs/scripts/tor_toggle.sh
@@ -5,8 +5,10 @@
 grep -q '^ *forward-socks5 *\/ *127.0.0.1:9050 \{1,\}\.' /etc/privoxy/config
 if [ $? = 0 ]; then
 	sudo sed -i 's/^\( *forward-socks5 *\/ *127.0.0.1:9050 \{1,\}\.\)/#\1/' /etc/privoxy/config
+	echo "Tor OFF"
 else
 	sudo sed -i 's/^#\( *forward-socks5 *\/ *127.0.0.1:9050 \{1,\}\.\)/\1/' /etc/privoxy/config
+	echo "Tor ON"
 fi
 
 exit 0
