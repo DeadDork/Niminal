a64c4be16d45ada6a0cce4f92d07bd8e13d07784
DeadDork <nimrodomer@gmail.com>
2012-06-27 21:57:57 -0700

    Made tor_toggle a bit more user friendly.

--- customs/scripts/tor_toggle.sh
+++ customs/scripts/tor_toggle.sh
@@ -1,14 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 # A quickie script to toggle Tor.
 
-grep -q '^ *forward-socks5 *\/ *127.0.0.1:9050 \{1,\}\.' /etc/privoxy/config
-if [ $? = 0 ]; then
-	sudo sed -i 's/^\( *forward-socks5 *\/ *127.0.0.1:9050 \{1,\}\.\)/#\1/' /etc/privoxy/config
-	echo "Tor OFF"
+change_row=$(awk '/^ *#? *forward-socks5 *\/ *127.0.0.1:9050 +\./{print NR}' </etc/privoxy/config)
+
+clear
+if [[ "$(awk 'NR=='$change_row /etc/privoxy/config)" =~ ^\ *\# ]]; then
+	tor_status='OFF'
 else
-	sudo sed -i 's/^#\( *forward-socks5 *\/ *127.0.0.1:9050 \{1,\}\.\)/\1/' /etc/privoxy/config
-	echo "Tor ON"
+	tor_status='ON'
 fi
+echo "Tor is $tor_status"
+PS3='Toggle Tor?'
+select toggle in 'Yes' 'No'; do
+	case $toggle in
+		'Yes' )
+			if [ "${tor_status}" == 'OFF' ]; then
+				sudo bash -c "sed -i \"$change_row \"'s/^ *#\(.*\)/\1/' /etc/privoxy/config"
+				echo "Tor toggled ON."
+			elif [ "${tor_status}" == 'ON' ]; then
+				sudo bash -c "sed -i \"$change_row \"'s/\(.*\)/#\1/' /etc/privoxy/config"
+				echo "Tor toggled OFF."
+			fi
+			break
+			;;
+		'No' )
+			break
+			;;
+		* )
+			echo 'Bad entry. Try again.'
+			;;
+	esac
+done
 
 exit 0
